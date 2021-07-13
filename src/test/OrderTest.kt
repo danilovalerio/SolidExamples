c8c9bfc84ca0d677de3f2c06ae54d64365aba56b
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-13 18:11:06 -0300

    Teste para imposto em mes do cigarro

--- src/test/OrderTest.kt
+++ src/test/OrderTest.kt
@@ -43,6 +43,18 @@ class OrderTest {
     }
 
     @Test
+    fun `Deve calcular os impostos no mes comemorativo pedido`() {
+        val order = Order()
+        order.addItem(Cigar("Eight",8)) //0.1 = 0.8
+        order.addItem(Beer("Glacial",5)) //0.1 = 0.5
+        order.addItem(Water("Bem Barato",5)) // 0.0
+        val impostos = order.getTaxes()
+        val expected = 1.3
+
+        assertEquals(expected, impostos)
+    }
+
+    @Test
     fun `Deve calcular o total do pedido`() {
         val order = Order()
         order.addItem(Cigar("Eight",8)) //0.2 = 1.6
