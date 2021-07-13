deec4bae2e2ab2712a7c6120b164e15afbe98e0d
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-13 18:08:36 -0300

    Aqui esta com uma dependencia interna

--- src/Cigar.kt
+++ src/Cigar.kt
@@ -1,8 +1,15 @@
+import java.util.*
 
 class Cigar(brand: String, price: Int) : TaxItem(category = "Cigar", brand = brand, price = price) {
     override val price: Int = price
 
     override fun getTax(): Double {
+        val today = Date() //TODO: Dependencia interna, nao tem como testar
+        //Num mês específico o imposto do cigarro será mais para motivar o consumo
+        if (today.month === 0) {
+            return 0.1
+        }
+
         return 0.2
     }
 }
