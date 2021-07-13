bd7b4b8a1893124eaca6f9559c51537fedbf7611
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-13 14:28:08 -0300

    refactor name to category

--- src/Item.kt
+++ src/Item.kt
@@ -1,16 +1,16 @@
 data class Item(
-    val name: String,
+    val category: String,
     val brand: String,
     val price: Int
 ) {
 
     fun calculateTaxes(): Double {
         var taxes = 0.0
-        if (this.name == "Cigar") {
+        if (this.category == "Cigar") {
             taxes += this.price * 0.2
         }
 
-        if (this.name == "Beer") {
+        if (this.category == "Beer") {
             taxes += this.price * 0.1
         }
 
